56b7d1c670f2be84c2a31a0ae5de5bceb6ab019e
Troy Giorshev <troygiorshev@gmail.com>
2019-01-19 13:52:43 -0500

    Start scrape.py

---
+++ scrape.py
@@ -0,0 +1,7 @@
+'''
+Webscraper module.  Everything we need to scrape the Yelp data, putting everything we want into a Pandas dataframe.
+
+These functions should be imported by our main program.
+'''
+
+import requests
