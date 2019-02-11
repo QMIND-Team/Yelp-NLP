4123a5dac1de54fe64f32a571bf924b5bf0292b3
Olivia <16kot1@queensu.ca>
2019-02-10 21:39:56 -0500

    finalized both multi-index configs

--- dataFrameStruct.py
+++ dataFrameStruct.py
@@ -5,7 +5,7 @@ import scrape as sc
 url1 = "https://www.yelp.ca/biz/harpers-burger-bar-kingston"
 url2 = "https://www.yelp.ca/biz/meridian-credit-union-toronto-4"
 
-col = ["Reviews","Sentiment" , "Url"]
+col = ["Reviews","Sentiment", "Url", "Average Review", "Average Star"]
 
 revs1 = pd.DataFrame(columns = col)
 revs2 = pd.DataFrame(columns = col)
@@ -14,11 +14,11 @@ revs1 = sc.addToRevs(revs1, url1)
 revs2 = sc.addToRevs(revs2, url2)
 
 
-'''
+
 #Companies and rev # on side, Reviews on top
 df = pd.concat([revs1, revs2], keys=['Harpers', 'Credit Union'], names=['Company', 'Index'])
 print(df)
-'''
+
 #Companies and sections on top
-df = pd.concat([revs1, revs2], axis = 1, keys=['Harpers', 'Credit Union'], names=['Company', 'Index'])
+df = pd.concat([revs1, revs2], axis = 1, keys=['Harpers', 'Credit Union'], names=['Company', 'Category'])
 print(df)
