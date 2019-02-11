de4e5fa81c439e19b9ab48af73c53cb54504c83b
Olivia <16kot1@queensu.ca>
2019-02-10 21:26:06 -0500

    created multi-index df with companies on vertical axis

--- dataFrameStruct.py
+++ dataFrameStruct.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 import scrape as sc
 
-
 url1 = "https://www.yelp.ca/biz/harpers-burger-bar-kingston"
 url2 = "https://www.yelp.ca/biz/meridian-credit-union-toronto-4"
 revs1 = pd.DataFrame(columns=["Reviews"])
@@ -12,8 +11,11 @@ revs1 = sc.addToRevs(revs1, url1)
 revs2 = sc.addToRevs(revs2, url2)
 
 col = ["Reviews","Sentiment" , "Url"]
-df = pd.DataFrame(columns = col)
-
-#df = pd.concat([df,revs], ignore_index= True)
+'''
+#Companies and rev # on side, Reviews on top
 df = pd.concat([revs1, revs2], keys=['Harpers', 'Credit Union'], names=['Company', 'Index'])
+print(df)
+'''
+#Companies and sections on top, simple index on side
+df = pd.concat([revs1, revs2], axis = 1, keys=['Harpers', 'Credit Union'], names=['Company', 'Index'])
 print(df)
