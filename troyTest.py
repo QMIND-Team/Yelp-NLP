095e3cefa8196ccfefc1e88fa79b3f583ff41620
Troy Giorshev <troygiorshev@gmail.com>
2019-03-03 13:23:52 -0500

    Spelling Error x2

--- troyTest.py
+++ troyTest.py
@@ -100,7 +100,7 @@ def main():
     revs = sc.mergeRevs(revslist, names)
 
     print(revs)
-    pp.print(revs)
+    pp.pprint(revs)
     pickle.dump(revs, open("df.p","wb"))
 
     #Testing wordCloud
