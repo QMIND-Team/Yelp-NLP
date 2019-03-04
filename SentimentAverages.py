3afd7610f367143996a0399d4db74112a6aa8961
Olivia <16kot1@queensu.ca>
2019-03-04 15:21:51 -0500

    convert dates to type datetime and sort data by date

--- SentimentAverages.py
+++ SentimentAverages.py
@@ -8,9 +8,20 @@ print(data)
 
 companies = list(data.columns.levels[0])
 
+sentAves = pd.DataFrame(columns = companies)
+#print(sentAves)
+
 #go through each company
 for company in companies:
-    #sort reviews by year
+    #convert Dates column to type datetime 
+    data[company,'Dates'] = pd.to_datetime(data[company,'Dates'])
+    #sort reviews in ascending order by date
+    data[company] = data[company].sort_values(by =['Dates']))
+
+    
+
+    years = []
+    #for year in years
 
     #find the average sentiment of that year
 
